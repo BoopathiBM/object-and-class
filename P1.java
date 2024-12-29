c2bc492edbc1e1a2b60d79c5c62572b81091be77
boopathiBM <boopathibm19.com>
2024-12-29 12:05:02 +0530

    calling method inside same class

---
+++ P1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class P1{    
+    void commingSoon(){
+        System.out.println("commin soon");
+    }                        
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        P1 india = new P1();
+        P1 usa = new P1();
+        india.commingSoon();
+    }
+}
+
